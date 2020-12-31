226fe3f1b9b715feb7fadbd8b448b68f5d9bb92b
Kevin Reyes <reyeskevin9767@gmail.com>
2020-12-31 00:03:32 -0500

    learn to handle mutiple inputs

--- gfl/src/App.js
+++ gfl/src/App.js
@@ -1,42 +1,60 @@
-import React, { useState, useEffect } from 'react';
-import { dolls, equips, fairies } from 'girlsfrontline-core';
+import React, { useState, useReducer } from 'react';
+import { dolls } from 'girlsfrontline-core';
 
 const App = () => {
-  const onInput = (dollName, dollLevel) => {
-    const doll = dolls.find(({ codename }) => codename === dollName);
-    doll.level = dollLevel;
+  const onInput = (name, level) => {
+    const doll = dolls.find(({ codename }) => codename === name);
+
+    doll.level = 1;
+    doll.favor = 0;
+    doll.dummyLink= 1;
     console.log(doll.stats);
-    return doll;
+
+    setdollInfo(doll);
   };
 
   const onSubmit = (event) => {
     event.preventDefault();
-
-    onInput(term, level);
+    onInput(userInput.dollName, parseInt(userInput.dollLevel));
   };
 
-  const [term, setTerm] = useState('');
-  const [level, setLevel] = useState(4);
+  const [userInput, setUserInput] = useReducer(
+    (state, newState) => ({ ...state, ...newState }),
+    {
+      dollName: 'G36',
+      dollLevel: '4',
+    }
+  );
 
-  // g36.level = 1;
-  // g36.dummyLink = 3;
-  // g36.favor = 50;
+  const handleChange = (evt) => {
+    const name = evt.target.name;
+    const newValue = evt.target.value;
+
+    setUserInput({ [name]: newValue });
+  };
+
+  const [dollInfo, setdollInfo] = useState({});
+
+  console.log(dollInfo);
 
   return (
     <div>
       <form onSubmit={onSubmit}>
         <label>
-          <input onChange={(e) => setTerm(e.target.value)} value={term} />
+          <input
+            onChange={handleChange}
+            name="dollName"
+            value={userInput.dollName}
+          />
         </label>
         <label>
           <input
-            type="number"
-            onChange={(e) => setLevel(e.target.value)}
-            value={level}
-            min="1"
-            max="100"
+            onChange={handleChange}
+            name="dollLevel"
+            value={userInput.dollLevel}
           />
         </label>
+        <button type="submit">Submit</button>
       </form>
     </div>
   );
